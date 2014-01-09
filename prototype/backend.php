5d023e373c344d04876caf9b4408d13a60975e14
frootmig <voja@voja.de>
2014-01-09 10:25:53 +0100

    demo implementation for zone list inquire

---
+++ prototype/backend.php
@@ -0,0 +1,42 @@
+<?
+
+// --- config start -> move to config.inc.php ? ---
+
+// OpenDNSSEC configuration file containing the zone list
+$zonelist = 'zonelist.xml';
+
+// --- config end ---
+
+// action: list all zones
+
+// Load and parse XML
+$xml = simplexml_load_file($zonelist);
+
+// List all zone names
+$result = $xml->xpath('/ZoneList/Zone');
+
+print_r($result);
+
+// New result array
+$zone_list = array();
+
+// Walk all zone tags
+foreach ($result as $r) {
+	// extract zone name
+	$zone_name = (string)$r->attributes()['name'];
+	// add to result
+	$zone_list[$zone_name] = $r;
+}
+
+// sort asc
+ksort($zone_list);
+
+// debug: dump data structure
+print_r($zone_list);
+
+// test: output as json
+echo json_encode($zone_list);
+
+// TODO: one file per action? or one file calling methods?
+
+?>
